38326a70c60f942ea44b8d121bac4aa423866bfb
konradsiekaczz <siekaczkonrad@gmail.com>
2021-03-14 19:24:05 +0100

    Initial commit

---
+++ src/main/java/com/freetable/freetable/FreeTableApplication.java
@@ -0,0 +1,13 @@
+package com.freetable.freetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/freetable/freetable/FreeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freetable.freetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
