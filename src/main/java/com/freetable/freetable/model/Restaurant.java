36bd8969528b240422b27bdef551853d820e8f5c
konradsiekaczz <siekaczkonrad@gmail.com>
2021-03-14 20:05:20 +0100

    Add class for restaurant

---
+++ src/main/java/com/freetable/freetable/model/Restaurant.java
@@ -0,0 +1,20 @@
+package com.freetable.freetable.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "restaurant")
+public class Restaurant {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+    private String name;
+    @OneToMany(mappedBy = "restaurant")
+    private List<Board> boardList;
+}
