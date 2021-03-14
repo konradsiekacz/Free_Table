b552cd271e6b5b9438abeaf5295a44b75e1d75b5
konradsiekaczz <siekaczkonrad@gmail.com>
2021-03-14 20:23:50 +0100

    Create class for reservation and add relations for classes

--- src/main/java/com/freetable/freetable/model/Reservation.java
+++ src/main/java/com/freetable/freetable/model/Reservation.java
@@ -8,17 +8,18 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
-@Table(name = "board")
-public class Board {
+@Table(name = "reservation")
+public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private long id;
-    private long numberOfSeats;
-    private boolean isReserved;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
     @ManyToOne
     @JoinColumn(name = "restaurant_id", nullable = false)
     private Restaurant restaurant;
-    @OneToOne(mappedBy = "board")
-    @JoinColumn(name = "reservation_id")
-    private Reservation reservation;
+    @OneToOne
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
 }
--- src/main/java/com/freetable/freetable/model/User.java
+++ src/main/java/com/freetable/freetable/model/User.java
@@ -3,10 +3,8 @@ package com.freetable.freetable.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,4 +14,6 @@ public class User {
     @Id
     private long id;
     private String name;
+    @OneToMany(mappedBy = "users")
+    private List<Reservation> reservationList;
 }
